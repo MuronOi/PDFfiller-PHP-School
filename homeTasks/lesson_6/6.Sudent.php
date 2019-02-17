28dd4bd0271f95ed1e11244f5341640be102d0af
MuronOi <muronchukoleg@gmail.com>
2019-02-17 17:06:28 +0200

    Add Exercise 1 4 and some magic

--- homeTasks/lesson_6/6.Sudent.php
+++ homeTasks/lesson_6/6.Sudent.php
@@ -1,12 +1,5 @@
 <?php
 
-//Use your objects from above and let each one of the 5 students study for 60, 100, 40, 300, 1000 minutes, respectively.
-// So the first student studies 60 minutes, the second studies 100 minutes, etc. After that call the showMyself methods
-// on all 5 again and check whether their new gpa reflects how much they studied.
-
-//Well, now use magic methods from the link http://php.net/manual/en/language.oop5.magic.php
-// and try to refactor your code using these methods. Imagine additional cases for your code if needed.
-
 class Student
 {
     private $firstname;
@@ -53,7 +46,7 @@ class Student
      * @param mixed $gender
      * @throws Exception
      */
-    public function setGender($gender)
+    protected function setGender($gender)
     {
         try {
             if (($gender === 'male') || ($gender === 'female')) {
@@ -71,7 +64,7 @@ class Student
     /**
      * @param mixed $status
      */
-    public function setStatus($status)
+    protected function setStatus($status)
     {
         $statusesList = ['freshman', 'sophomore', 'junior', 'senior'];
         $this->status = in_array($status, $statusesList, 1) ? $status : 'null';
@@ -80,11 +73,14 @@ class Student
     /**
      * @param mixed $gpa
      */
-    public function setGpa($gpa)
+    protected function setGpa($gpa)
     {
         $this->gpa = ($gpa >= 0 && $gpa <= 4.0) ? $gpa : 0 ;
     }
 
+    /**
+     *Shows all information about the Student
+     */
     public function showMyself()
     {
         echo 'Student ', $this->firstname, ' ', $this->lastname, ', have a gender - ', $this->gender, PHP_EOL;
@@ -101,6 +97,30 @@ class Student
             ? 4.0
             : round(($this->gpa + log($study_time/60)),2 );
     }
+
+    public function __toString()
+    {
+        return 'Student ' . $this->firstname . ' ' . $this->lastname . ', ' . $this->gender . PHP_EOL .
+            'Status - ' . $this->status . ', GPA is ' . number_format($this->gpa,2) . PHP_EOL;
+    }
+
+
+    public function __get($some)
+    {
+        if (property_exists($this, $some)) {
+            return $this->$some;
+        }
+    }
+
+    public function __invoke($study_time)
+    {
+        $this->gpa = ($this->gpa + log($study_time/60)) > 4.0
+            ? 4.0
+            : round(($this->gpa + log($study_time/60)),2 );
+        return $this->gpa;
+    }
+
+
 }
 
 $studentsList = [
@@ -115,10 +135,10 @@ $student = [];
 
 for ($i = 0; $i < count($studentsList); $i++  ) {
     $student[$i] = new Student(($studentsList[$i][0]),
-                           ($studentsList[$i][1]),
-                           ($studentsList[$i][2]),
-                           ($studentsList[$i][3]),
-                           ($studentsList[$i][4]));
+                               ($studentsList[$i][1]),
+                               ($studentsList[$i][2]),
+                               ($studentsList[$i][3]),
+                               ($studentsList[$i][4]));
 }
 
 foreach ($student as $key => $value) {
@@ -136,3 +156,12 @@ foreach ($student as $key => $value) {
     $value->showMyself();
     echo PHP_EOL;
 }
+
+echo $student[1]->gpa, PHP_EOL; // __get
+
+echo $student[1]; // __toString
+
+$workingTime = 100000;
+echo 'Student ', $student[1]->firstname, ' ', $student[1]->lastname, "worked very hard and till $workingTime minutes and changed GPA to ",
+    $student[1]($workingTime), PHP_EOL ; // __invoke + __get
+
