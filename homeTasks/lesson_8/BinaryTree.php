cc5dea612cae80e8c74aee6dc90b83099cfe8a17
MuronOi <muronchukoleg@gmail.com>
2019-02-26 00:53:00 +0200

    La*a

--- homeTasks/lesson_8/BinaryTree.php
+++ homeTasks/lesson_8/BinaryTree.php
@@ -1,7 +1,67 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: muronoi
- * Date: 24.02.19
- * Time: 21:28
- */
+
+require_once __DIR__.'/SingleLeaf.php';
+
+class BinaryTree extends SingleLeaf
+{
+    private $root = null;
+
+    /**
+     * BinaryTree constructor.
+     * @param null $root
+     */
+    public function __construct($value)
+    {
+        $leaf = new SingleLeaf($value);
+        $leaf->setTop(null);
+        $this->root = $leaf;
+
+    }
+
+    public function append($value){
+        $newLeaf = new SingleLeaf();
+        $newLeaf->setValue($value);
+
+//        if (empty($this->root)){
+//            $this->root = $newLeaf;
+//        } elseif () {
+//
+//        }
+    }
+
+    public function isEmpty ()
+    {
+        return is_null($this->root);
+    }
+
+    public function insert ($value)
+    {
+        $leaf = new SingleLeaf($value);
+        $this->insertNode($leaf, $this->root);
+    }
+
+    protected function insertNode (SingleLeaf $node, $subtree)
+    {
+        if (is_null($subtree)) {
+            $subtree = $node;
+        } else {
+            if ( is_null($subtree->getLeft())){
+                $this->insertNode($node, $subtree->getLeft());
+                $subtree->setLeft($node);
+            } elseif (is_null($subtree->getRight())) {
+                $this->insertNode($node, $subtree->getRight());
+                $subtree->setRight($node);
+            }
+        }
+        return $this;
+    }
+}
+
+$obj = new BinaryTree(1);
+$obj->insert(2);
+$obj->insert(3);
+$obj->insert(4);
+
+
+var_export($obj);
+
--- homeTasks/lesson_8/SingleLeaf.php
+++ homeTasks/lesson_8/SingleLeaf.php
@@ -1,7 +1,86 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: muronoi
- * Date: 24.02.19
- * Time: 21:26
- */
+
+class SingleLeaf
+{
+    private $value;
+    private $left = null;
+    private $right = null;
+    private $top = null;
+
+    /**
+     * SingleLeaf constructor.
+     * @param $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param null $left
+     */
+    public function setLeft($left): void
+    {
+        $this->left = $left;
+    }
+
+    /**
+     * @param null $right
+     */
+    public function setRight($right): void
+    {
+        $this->right = $right;
+    }
+
+    /**
+     * @param null $top
+     */
+    public function setTop($top): void
+    {
+        $this->top = $top;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return null
+     */
+    public function getLeft()
+    {
+        return $this->left;
+    }
+
+    /**
+     * @return null
+     */
+    public function getRight()
+    {
+        return $this->right;
+    }
+
+    /**
+     * @return null
+     */
+    public function getTop()
+    {
+        return $this->top;
+    }
+
+
+
+}
